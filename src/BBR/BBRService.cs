3695ac574e34722056c4e4277a68faf5a5e0281e
Mihai <mihai.tirtara@gmail.com>
2020-11-18 15:07:39 +0100

    Added method for processing the BBR files
    Signed-off-by: Mihai <mihai.tirtara@gmail.com>

--- src/BBR/BBRService.cs
+++ src/BBR/BBRService.cs
@@ -4,6 +4,9 @@ using Datafordelen.Ftp;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Datafordelen.BBR
 {
@@ -22,9 +25,57 @@ namespace Datafordelen.BBR
             _producer = kakfkaProducer;
         }
 
-        public async Task  GetBBRData()
+        public async Task GetBBRData()
         {
-            await _client.GetFileFtp(_appSettings.FtpServer, _appSettings.BBRUserName, _appSettings.BBRPassword, _appSettings.BBRUnzipPath,_appSettings.BBRUnzipPath);
+            await _client.GetFileFtp(_appSettings.FtpServer, _appSettings.BBRUserName, _appSettings.BBRPassword, _appSettings.BBRUnzipPath, _appSettings.BBRUnzipPath);
+            await ProcessBBRFiless(
+                _appSettings.BBRUnzipPath,
+                _appSettings.BBRProcessedPath,
+                _appSettings.MinX,
+                _appSettings.MinY,
+                _appSettings.MaxX,
+                _appSettings.MaxY
+            );
+        }
+
+        private async Task ProcessBBRFiless(string sourceDirectory, string destinationDirectory, double minX, double minY, double maxX, double maxY)
+        {
+            var destinfo = new DirectoryInfo(destinationDirectory);
+            if (destinfo.Exists == false)
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+            var sourceinfo = new DirectoryInfo(sourceDirectory);
+            var dirs = sourceinfo.GetDirectories();
+
+            var fileEntries = Directory.GetFiles(sourceDirectory).ToList();
+            foreach (string fileName in fileEntries)
+            {
+                if (!fileName.Contains("Metadata") && !fileName.Contains(".zip"))
+                {
+                    await BBRToKafka(fileName, minX, minY, maxX, maxY);
+                    var file = Path.GetFileName(fileName);
+                    var destFile = Path.Combine(destinationDirectory, file);
+                    File.Move(fileName, destFile);
+                    _logger.LogInformation(fileName + " moved in " + destFile);
+                }
+                else if (fileName.Contains("Metada"))
+                {
+                    var file = Path.GetFileName(fileName);
+                    var destFile = Path.Combine(destinationDirectory, file);
+                    File.Move(fileName, destFile);
+                    _logger.LogInformation(fileName + " moved in " + destFile);
+                }
+                else
+                {
+                    _logger.LogInformation("Zip file still in folder");
+                }
+            }
+        }
+
+        private async Task BBRToKafka(string filename, double minX, double minY, double maxX, double maxY)
+        {
+            
         }
     }
 }
